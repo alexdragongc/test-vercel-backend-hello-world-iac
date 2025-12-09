0a3bf0ee0ff37cb9740275cbf89e41c57fddca6f
alexangc <alexis.miranda@hotmail.fr>
2025-12-09 16:45:43 +0100

    Init

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+const app = express();
+
+app.use(json());
+
+app.get("/", (_req, res) => {
+  res.send("Hello, World! This is a basic Express app deployed on Vercel.");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from the API!" });
+});
+
+export default app; // Export for Vercel serverless function
