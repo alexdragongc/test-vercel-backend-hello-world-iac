04467b3aa079ed4843586da700b8af4322124274
alexangc <alex.miranda@dragongc.com>
2026-01-22 16:29:23 +0100

    Import the existing project's config

---
+++ .infrastructure/generated_resources.tf
@@ -0,0 +1,59 @@
+# __generated__ by OpenTofu
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by OpenTofu
+resource "vercel_project" "my_project" {
+  auto_assign_custom_domains                        = true
+  automatically_expose_system_environment_variables = false
+  build_command                                     = null
+  build_machine_type                                = null
+  customer_success_code_visibility                  = false
+  dev_command                                       = "npm run dev"
+  directory_listing                                 = false
+  enable_affected_projects_deployments              = false
+  enable_preview_feedback                           = null
+  enable_production_feedback                        = false
+  environment                                       = null
+  framework                                         = "express"
+  function_failover                                 = false
+  git_comments                                      = null
+  git_fork_protection                               = true
+  git_lfs                                           = false
+  git_provider_options                              = null
+  git_repository = {
+    deploy_hooks      = null
+    production_branch = "master"
+    repo              = "alexangc/test-vercel-backend-hello-world-iac"
+    type              = "github"
+  }
+  ignore_command  = null
+  install_command = null
+  name            = "my-express-backend"
+  node_version    = "22.x"
+  oidc_token_config = {
+    issuer_mode = "team"
+  }
+  on_demand_concurrent_builds             = false
+  options_allowlist                       = null
+  output_directory                        = null
+  password_protection                     = null
+  preview_deployment_suffix               = null
+  preview_deployments_disabled            = false
+  prioritise_production_builds            = false
+  protection_bypass_for_automation        = null
+  protection_bypass_for_automation_secret = null # sensitive
+  public_source                           = null
+  resource_config = {
+    fluid                     = true
+    function_default_cpu_type = null
+    function_default_regions  = ["iad1"]
+    function_default_timeout  = null
+  }
+  root_directory  = null
+  skew_protection = null
+  team_id         = null
+  trusted_ips     = null
+  vercel_authentication = {
+    deployment_type = "standard_protection_new"
+  }
+}
---
+++ .infrastructure/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = vercel_project.my_project
+  id = "prj_LNiPwjtv3VyAzq23rCnWH0t10MCi" # e.g., prj_abc123
+}
