99eecb55c6523e57589a245db130935aa076a2c5
alexangc <alex.miranda@dragongc.com>
2026-01-30 15:58:50 +0100

    Extract backend config into shared directory

--- .infrastructure/web1/backend.tf
+++ .infrastructure/web1/backend.tf
@@ -3,24 +3,5 @@ terraform {
   # AWS_ACCESS_KEY_ID="<your_access_key>"
   # AWS_SECRET_ACCESS_KEY="<your_secret_key>"
   required_version = "~> 1.11"
-  backend "s3" {
-    endpoints = {
-      s3 = "https://sfo3.digitaloceanspaces.com"
-    }
-
-    bucket = "opentofu-states"
-    key    = "${var.project_name}/${var.environment}/terraform.tfstate"
-
-    # Deactivate a few AWS-specific checks
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_s3_checksum            = true
-    region                      = "sfo3"
-
-    # Enable state locking with a lockfile
-    use_lockfile = true
-
-  }
+  backend "s3" {}
 }
