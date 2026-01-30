72296a31ba915c85547d47ce010094f4e4513111
alexangc <alex.miranda@dragongc.com>
2026-01-30 15:22:39 +0100

    Migrate state to `project_name/environment`

--- .infrastructure/web1/backend.tf
+++ .infrastructure/web1/backend.tf
@@ -9,7 +9,7 @@ terraform {
     }
 
     bucket = "opentofu-states"
-    key    = "${var.environment}/terraform.tfstate"
+    key    = "${var.project_name}/${var.environment}/terraform.tfstate"
 
     # Deactivate a few AWS-specific checks
     skip_credentials_validation = true
---
+++ .infrastructure/web1/common.auto.tfvars
@@ -0,0 +1,2 @@
+# *.auto.tfvars files are automatically loaded even if not passed in the CLI
+project_name = "web1"
